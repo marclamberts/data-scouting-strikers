bea42a6bc2ed527c9f9c8cf84b29fe971a9b9fc0
Marc Lamberts <43936834+marclamberts@users.noreply.github.com>
2023-09-28 21:18:45 +0200

    Update datascouting.py

--- datascouting.py
+++ datascouting.py
@@ -15,15 +15,15 @@ def load_and_process_data(file_path):
     # Drop duplicate columns from the DataFrame
     df = df.loc[:, ~df.columns.duplicated()]
 
-    # Calculate percentile ranks for the specified offensive metrics based on the total dataset and convert to 100.0 scale
-    percentile_ranks = pd.DataFrame()
-    for col in offensive_metrics:
-        percentile_ranks[f"{col} Percentile Rank"] = df[col].rank(pct=True) * 100.0
-
-    # Concatenate the percentile ranks DataFrame with the original DataFrame
-    df = pd.concat([df, percentile_ranks], axis=1)
-
     return df
+
+def calculate_percentile_ranks(data, metrics):
+    # Calculate percentile ranks for the specified metrics based on the selected data and convert to 100.0 scale
+    percentile_ranks = pd.DataFrame()
+    for col in metrics:
+        percentile_ranks[f"{col} Percentile Rank"] = data[col].rank(pct=True) * 100.0
+
+    return percentile_ranks
 
 def main():
     st.title("Men strikers 2022-2023")
@@ -35,25 +35,37 @@ def main():
     file_path = "Complete database.xlsx"
     df = load_and_process_data(file_path)
 
-    # Add a text input for the user to search for a specific player
+    # Create a filter to select the team, league, or all data
+    data_selection = st.sidebar.radio("Select Data to Calculate Percentile Ranks", ["All Data", "By Team", "By League"])
+
+    if data_selection == "By Team":
+        selected_team = st.sidebar.selectbox("Select Team", ["All Teams"] + df["Team within selected timeframe"].unique().tolist())
+        if selected_team != "All Teams":
+            df = df[df["Team within selected timeframe"] == selected_team]
+    elif data_selection == "By League":
+        selected_league = st.sidebar.selectbox("Select League", ["All Leagues"] + df["League"].unique().tolist())
+        if selected_league != "All Leagues":
+            df = df[df["League"] == selected_league]
+
+    # Filter the data based on the search query
     search_player = st.text_input("Search Player", "")
-
-    # Filter the data based on the search query and display a bar chart for the selected player
     if search_player:
-        filtered_df = df[df["Player"].str.contains(search_player, case=False)]
-        if not filtered_df.empty:
-            player_name = filtered_df.iloc[0]["Player"]
-            player_metrics = filtered_df[[f"{metric} Percentile Rank" for metric in offensive_metrics] + ['Player']].melt(id_vars=['Player'], var_name='Metric', value_name='Percentile Rank')
+        df = df[df["Player"].str.contains(search_player, case=False)]
 
-            # Create a bar chart for the selected player's offensive metrics
-            bar_chart = alt.Chart(player_metrics).mark_bar().encode(
-                x=alt.X('Percentile Rank:Q', title='Percentile Rank', axis=alt.Axis(format='d')),
-                y=alt.Y('Metric:N', title='Metric', sort=alt.EncodingSortField(field="Percentile Rank", op="mean", order="descending")),
-                tooltip=['Metric', 'Percentile Rank']
-            ).properties(width=800, height=600, title=f'{player_name} - Mean Percentile Ranks for Offensive Metrics |@ShePlotsFC')
+    # Calculate percentile ranks for the selected data and metrics
+    percentile_ranks_df = calculate_percentile_ranks(df, offensive_metrics)
 
-            st.altair_chart(bar_chart)
-            st.write(player_metrics)
+    # Melt the DataFrame for visualization
+    melted_df = pd.melt(percentile_ranks_df, var_name='Metric', value_name='Percentile Rank')
+
+    # Create a bar chart for the selected player's offensive metrics
+    bar_chart = alt.Chart(melted_df).mark_bar().encode(
+        x=alt.X('Percentile Rank:Q', title='Percentile Rank', axis=alt.Axis(format='d')),
+        y=alt.Y('Metric:N', title='Metric', sort=alt.EncodingSortField(field="Percentile Rank", op="mean", order="descending")),
+        tooltip=['Metric', 'Percentile Rank']
+    ).properties(width=800, height=600, title=f'Mean Percentile Ranks for Offensive Metrics |@ShePlotsFC')
+
+    st.altair_chart(bar_chart)
 
     # Add the text at the bottom of the app
     st.markdown("Marc Lamberts @lambertsmarc @ShePlotsFC | Collected at 22-07-2023 | Wyscout")
