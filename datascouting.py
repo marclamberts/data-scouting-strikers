bce28c982508394f24a3a7a6170f42c1537b12b6
Marc Lamberts <43936834+marclamberts@users.noreply.github.com>
2023-09-28 16:47:50 +0200

    Update datascouting.py

--- datascouting.py
+++ datascouting.py
@@ -54,19 +54,24 @@ def main():
     # Create a dropdown for the user to select a metric category in the sidebar
     metric_category = st.sidebar.selectbox("Select Metric Category", ["Offensive", "Defensive", "Passing"])
 
-    # Check if the selected category is Offensive, Defensive, or Passing
-    if metric_category in ["Offensive", "Defensive", "Passing"]:
-        # Calculate the mean percentile rank for each metric in the selected category
-        category_metrics = [col for col in df.columns if metric_category in col]
-        mean_percentiles = df[category_metrics].mean()
+    # Define the offensive metrics
+    offensive_metrics = [
+        'Goals per 90', 'Non-penalty goals per 90', 'Shots per 90', 'xG per 90', 'Assists per 90', 'xA per 90',
+        'Crosses per 90', 'Dribbles per 90', 'Offensive duels per 90', 'Touches in box per 90', 'Progressive runs per 90'
+    ]
 
-        # Create a bar chart for percentile ranks
+    # Check if the selected category is Offensive and display the bar chart for offensive metrics
+    if metric_category == "Offensive":
+        # Calculate the mean percentile rank for each offensive metric
+        mean_percentiles = df[offensive_metrics].mean()
+
+        # Create a bar chart for percentile ranks of offensive metrics
         bar_chart = alt.Chart(pd.DataFrame({'Metric': mean_percentiles.index,
                                             'Mean Percentile Rank': mean_percentiles.values})).mark_bar().encode(
             x=alt.X('Metric:N', title='Metric'),
             y=alt.Y('Mean Percentile Rank:Q', title='Mean Percentile Rank'),
             tooltip=['Metric', 'Mean Percentile Rank']
-        ).properties(width=800, height=600, title=f'Mean Percentile Ranks for {metric_category} Metrics |@ShePlotsFC')
+        ).properties(width=800, height=600, title=f'Mean Percentile Ranks for Offensive Metrics |@ShePlotsFC')
 
         st.altair_chart(bar_chart)
 
@@ -78,16 +83,16 @@ def main():
         filtered_df = df[df["Player"].str.contains(search_player, case=False)]
         if not filtered_df.empty:
             player_name = filtered_df.iloc[0]["Player"]
-            player_metrics = filtered_df[category_metrics].mean()
+            player_metrics = filtered_df[offensive_metrics].mean()
 
-            # Create a bar chart for the selected player's metrics
+            # Create a bar chart for the selected player's offensive metrics
             player_bar_chart = alt.Chart(pd.DataFrame({'Metric': player_metrics.index,
                                                        'Player': [player_name] * len(player_metrics),
                                                        'Mean Percentile Rank': player_metrics.values})).mark_bar().encode(
                 x=alt.X('Metric:N', title='Metric'),
                 y=alt.Y('Mean Percentile Rank:Q', title='Mean Percentile Rank'),
                 tooltip=['Metric', 'Mean Percentile Rank']
-            ).properties(width=800, height=600, title=f'{player_name} - Mean Percentile Ranks for {metric_category} Metrics |@ShePlotsFC')
+            ).properties(width=800, height=600, title=f'{player_name} - Mean Percentile Ranks for Offensive Metrics |@ShePlotsFC')
 
             st.altair_chart(player_bar_chart)
 
