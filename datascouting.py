fb6e67364fcd53ef34c82ff5d17c24c3124bf1ef
Marc Lamberts <43936834+marclamberts@users.noreply.github.com>
2023-11-02 19:34:54 +0100

    Update datascouting.py

--- datascouting.py
+++ datascouting.py
@@ -29,68 +29,76 @@ def main():
     search_players_button = st.sidebar.button("Search Players")
 
     if search_players_button:
+        st.experimental_set_query_params(page="search")
+    else:
+        st.experimental_set_query_params(page="main")
+    
+    if st.experimental_get_query_params()["page"] == "search":
         search_players()
     else:
-        # Set the minimal minutes to 500 using the slider in the sidebar
-        min_minutes_played = st.sidebar.slider("Minimum Minutes Played", min_value=0, max_value=2000, value=500, step=100)
+        show_main_page()
 
-        # Load data using the caching function
-        file_path = "Scouting database 31-10-2023.xlsx"
-        df = load_and_process_data(file_path)
+def show_main_page():
+    # Set the minimal minutes to 500 using the slider in the sidebar
+    min_minutes_played = st.sidebar.slider("Minimum Minutes Played", min_value=0, max_value=2000, value=500, step=100)
 
-        # Create a dropdown for the user to select a league in the sidebar
-        leagues = df["League"].unique()
-        selected_league = st.sidebar.selectbox("Select League", leagues)
+    # Load data using the caching function
+    file_path = "Scouting database 31-10-2023.xlsx"
+    df = load_and_process_data(file_path)
 
-        # Create a dropdown for the user to select a team within the selected league in the sidebar
-        teams_in_selected_league = df[df["League"] == selected_league]["Team within selected timeframe"].unique()
-        selected_team = st.sidebar.selectbox("Select Team", teams_in_selected_league, key="team_filter")
+    # Create a dropdown for the user to select a league in the sidebar
+    leagues = df["League"].unique()
+    selected_league = st.sidebar.selectbox("Select League", leagues)
 
-        # Calculate percentile ranks for all metrics based on the filtered dataset and convert to 100.0 scale
-        filtered_df = df[(df["League"] == selected_league) & (df["Team within selected timeframe"] == selected_team) & (df["Minutes played"] >= min_minutes_played)]
+    # Create a dropdown for the user to select a team within the selected league in the sidebar
+    teams_in_selected_league = df[df["League"] == selected_league]["Team within selected timeframe"].unique()
+    selected_team = st.sidebar.selectbox("Select Team", teams_in_selected_league, key="team_filter")
 
-        # Create a dropdown for the user to select a metric category in the sidebar
-        metric_category = st.sidebar.selectbox("Select Metric Category", ["Offensive", "Defensive", "Passing"])
+    # Calculate percentile ranks for all metrics based on the filtered dataset and convert to 100.0 scale
+    filtered_df = df[(df["League"] == selected_league) & (df["Team within selected timeframe"] == selected_team) & (df["Minutes played"] >= min_minutes_played)]
 
-        # Determine the relevant metrics based on the selected category
-        if metric_category == "Offensive":
-            relevant_metrics = [
-                'Goals per 90', 'Non-penalty goals per 90', 'Shots per 90', 'xG per 90', 'Assists per 90', 'xA per 90',
-                'Crosses per 90', 'Dribbles per 90', 'Offensive duels per 90', 'Touches in box per 90',
-                'Progressive runs per 90'
-            ]
-        elif metric_category == "Defensive":
-            relevant_metrics = [
-                'Defensive duels per 90', 'Defensive duels won, %', 'Aerial duels per 90', 'Aerial duels won, %', 
-                'Shots blocked per 90', 'PAdj Sliding tackles', 'PAdj Interceptions', 'Fouls per 90'
-            ]
-        else:
-            relevant_metrics = [
-                'Passes per 90', 'Accurate passes, %', 'Assists per 90', 'xA per 90', 'Second assists per 90',
-                'Third assists per 90', 'Key passes per 90', 'Passes to final third per 90', 'Passes to penalty area per 90',
-                'Through passes per 90', 'Deep completions per 90', 'Progressive passes per 90'
-            ]
+    # Create a dropdown for the user to select a metric category in the sidebar
+    metric_category = st.sidebar.selectbox("Select Metric Category", ["Offensive", "Defensive", "Passing"])
 
-        # Sort the filtered DataFrame based on the selected metric in descending order
-        selected_metric = st.sidebar.selectbox(f"Select {metric_category} Metric", relevant_metrics)
+    # Determine the relevant metrics based on the selected category
+    if metric_category == "Offensive":
+        relevant_metrics = [
+            'Goals per 90', 'Non-penalty goals per 90', 'Shots per 90', 'xG per 90', 'Assists per 90', 'xA per 90',
+            'Crosses per 90', 'Dribbles per 90', 'Offensive duels per 90', 'Touches in box per 90',
+            'Progressive runs per 90'
+        ]
+    elif metric_category == "Defensive":
+        relevant_metrics = [
+            'Defensive duels per 90', 'Defensive duels won, %', 'Aerial duels per 90', 'Aerial duels won, %', 
+            'Shots blocked per 90', 'PAdj Sliding tackles', 'PAdj Interceptions', 'Fouls per 90'
+        ]
+    else:
+        relevant_metrics = [
+            'Passes per 90', 'Accurate passes, %', 'Assists per 90', 'xA per 90', 'Second assists per 90',
+            'Third assists per 90', 'Key passes per 90', 'Passes to final third per 90', 'Passes to penalty area per 90',
+            'Through passes per 90', 'Deep completions per 90', 'Progressive passes per 90'
+        ]
 
-        sorted_df = filtered_df.sort_values(by=selected_metric, ascending=False)
+    # Sort the filtered DataFrame based on the selected metric in descending order
+    selected_metric = st.sidebar.selectbox(f"Select {metric_category} Metric", relevant_metrics)
 
-        # Display the team filter on the left column
-        st.sidebar.write(f"Selected Team: {selected_team}")
+    sorted_df = filtered_df.sort_values(by=selected_metric, ascending=False)
 
-        # Display the graph for the selected metric category in the main column
-        chart_data = sorted_df.head(15)
-        chart = alt.Chart(chart_data).mark_bar().encode(
-            x=alt.X(selected_metric, title=selected_metric),
-            y=alt.Y("Player", sort="-x", title="Player"),
-            tooltip=["Player", "Team", "Age", "Minutes played", selected_metric, alt.Tooltip(f"{selected_metric} Percentile Rank:Q", format=".1f")],
-            color=alt.Color(f"{selected_metric} Percentile Rank:Q", title=f"{selected_metric} Percentile Rank", scale=alt.Scale(scheme='viridis'))
-        ).properties(width=1000, height=600, title=f"Top 15 {metric_category} Performers in {selected_team} ({selected_league}) for {selected_metric} |@ShePlotsFC")
-        st.altair_chart(chart)
+    # Display the team filter on the left column
+    st.sidebar.write(f"Selected Team: {selected_team}")
 
-        # Add the text at the bottom of the app
-        st.markdown("Marc Lamberts @lambertsmarc @ShePlotsFC | Collected at 22-07-2023 | Wyscout")
+    # Display the graph for the selected metric category in the main column
+    chart_data = sorted_df.head(15)
+    chart = alt.Chart(chart_data).mark_bar().encode(
+        x=alt.X(selected_metric, title=selected_metric),
+        y=alt.Y("Player", sort="-x", title="Player"),
+        tooltip=["Player", "Team", "Age", "Minutes played", selected_metric, alt.Tooltip(f"{selected_metric} Percentile Rank:Q", format=".1f")],
+        color=alt.Color(f"{selected_metric} Percentile Rank:Q", title=f"{selected_metric} Percentile Rank", scale=alt.Scale(scheme='viridis'))
+    ).properties(width=1000, height=600, title=f"Top 15 {metric_category} Performers in {selected_team} ({selected_league}) for {selected_metric} |@ShePlotsFC")
+    st.altair_chart(chart)
+
+    # Add the text at the bottom of the app
+    st.markdown("Marc Lamberts @lambertsmarc @ShePlotsFC | Collected at 22-07-2023 | Wyscout")
 
 def search_players():
     st.title("Search Players")
@@ -100,7 +108,7 @@ def search_players():
         # Search for the player in the DataFrame and display their information
         player_info = df[df['Player'].str.contains(player_name, case=False, na=False)]
         if not player_info.empty:
-            st.write(player_info[['Player', 'Age', 'Team', 'Minutes played', 'Goals', 'Assists', 'xG', 'xA']])
+            st.write(player_info[['Player', 'Age', 'Team', 'Minutes played', 'Goals', 'Assists', 'xG', 'xA'])
         else:
             st.write("Player not found")
 
